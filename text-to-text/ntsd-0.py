cff7edc1f266614f850c55ce2ec4a14f24b657fd
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-06-14 09:59:58 +0700

    Updated

--- text-to-text/ntsd-0.py
+++ text-to-text/ntsd-0.py
@@ -9,23 +9,36 @@ def encode(text, key):
     l=0
     len_key = len(key)
     for c in text:
-        ciphertext += chr(abs(ord(c)-ord(key[l%len_key])))
+        #ciphertext += chr(ord(c)+ord(key[l%len_key]))
+        fullpart = "1"+str(ord(c)*ord(key[l%len_key]))
+        print(fullpart)
+        firstPart=int(fullpart[0::2])
+        endPart=int(fullpart[1::2])
+        #print(firstPart, chr(firstPart), endPart, chr(endPart))
+        ciphertext += chr(firstPart)+chr(endPart)
         l+=1
     return ciphertext
 def decode(ciphertext, key):
     text = ""
     l=0
     len_key = len(key)
-    for c in ciphertext:
-        text += chr(abs(ord(c)-ord(key[l%len_key])))
+    for i, j in zip(ciphertext[0::2], ciphertext[1::2]):
+        k = ord(key[l%len_key])
+        firstPart=str(ord(i))
+        endPart=str(ord(j))+" "
+        fullpart="".join(i+j for i, j in zip(firstPart, endPart))[1:]
+        print(fullpart)
+        text+= chr(int(fullpart)//k)
         l+=1
     return text
 
 if __name__ == '__main__':
-    text = "Hello you are so cute"
-    key = "สวัสดี"
+    text = "ควยไรสัส"
+    key = "ntsd"
+    print("text:"+text)
     ciphertext = encode(text, key)
-    text = decode(ciphertext, key)
     print("ciphertext:"+ciphertext)
+    text = decode(ciphertext, key)
+    
     print("text:"+text)
     
